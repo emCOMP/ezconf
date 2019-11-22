529e4fa5a9d9cb83d4fd259b7f0bd3e3ec0b811e
John Robinson <soco@uw.edu>
2019-11-21 19:25:42 -0800

    move __future__ line to top of file

--- ezconf/__init__.py
+++ ezconf/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 from os import path
 import json
-
-from __future__ import print_function
 
 version = '0.1'
 
